833df1d29ebd6d12625d8184447d8b65a3a1cece
Raif Ahmed <rahmed@redhat.com>
2019-10-07 13:32:13 +0200

    add the test certs

---
+++ ssl-certs/create-ssl-amq.sh
@@ -0,0 +1,25 @@
+rm -Rf ./ssl_certs
+mkdir ./ssl_certs
+cd ssl_certs
+set -x
+
+#URL will be <service_name>-<project_name>.openshift_master_default_subdomain
+export URL=broker-amq-headless-amq7-raif.apps.box.it-speeltuin.nl
+export KEYSTORE=nexus-keystore
+export TRUSTSTORE_PASSWORD=passw0rd
+export KEYSTORE_PASSWORD=passw0rd
+
+
+keytool -genkey -noprompt -keyalg RSA -alias amq-broker -dname "CN=${URL}" -keystore $KEYSTORE.jks -storepass $KEYSTORE_PASSWORD -keypass $KEYSTORE_PASSWORD -deststoretype pkcs12
+
+
+keytool -export -alias nexus -keystore ${KEYSTORE}.jks -storepass $KEYSTORE_PASSWORD -file ${KEYSTORE}_cert.der
+openssl x509 -inform DER -in ${KEYSTORE}_cert.der -out ${KEYSTORE}_cert.crt
+
+
+openssl pkcs12 -in $KEYSTORE.jks -nocerts -nodes  -out ${KEYSTORE}.key -passin pass:$KEYSTORE_PASSWORD
+
+
+#To convert a JKS (.jks) keystore to a PKCS12 (.p12) keystore
+keytool -importkeystore -srckeystore ${KEYSTORE}.jks -destkeystore ${KEYSTORE}.p12 -srcstoretype JKS -deststoretype PKCS12 -srcstorepass $KEYSTORE_PASSWORD -deststorepass $KEYSTORE_PASSWORD
+
